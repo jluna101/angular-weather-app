460ac44ed9d5a38bd8771e1e5469d68c3e56c172
jluna101 <jfluna1996@gmail.com>
2022-02-23 15:18:02 -0800

    Added display functionality and styling to SearchResults component

--- sei/labs/react-giphy-searcher/src/components/SearchResults.js
+++ sei/labs/react-giphy-searcher/src/components/SearchResults.js
@@ -1,5 +1,47 @@
-export default function SearchResults(){
+const SearchResults = ({ images }) => {
+    // return early if there are no images
+    if (!images.length) {
+      return <h2>No Images Found!</h2>
+    }
+  
     return (
-        <div>Hello from Search Results</div>
+      <div className="gallery">
+        {images.map(image => (
+          <div key={image.id} className="gif">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+export default SearchResults;
+
+
+
+
+
+    /*
+    const SearchResults = ({ images }) => {
+        // return early if there are no images
+        if (!images.length) {
+          return <h2>No Images Found!</h2>
+        }
+      
+        return (
+          <div className="gallery">
+            {images.map(image => (
+              <div key={image.id} className="gif">
+                <img src={image.url} />
+              </div>
+            ))}
+          </div>
+        )
+      }
+
+    return (
+        <div>Hello</div>
     )
 }
+
+*/
