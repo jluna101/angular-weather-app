00822f8b21beeccd4b033f947407a0a9aa16d0d3
jluna101 <jfluna1996@gmail.com>
2022-02-23 15:48:10 -0800

    Added the Giphy API request

--- sei/labs/react-giphy-searcher/src/components/SearchResults.js
+++ sei/labs/react-giphy-searcher/src/components/SearchResults.js
@@ -6,11 +6,13 @@ const SearchResults = ({ images }) => {
   
     return (
       <div className="gallery">
+
         {images.map(image => (
-          <div key={image.id} className="gif">
-            <img src={image.url} />
-          </div>
+        <div key={image.id} className="gif">
+            <img src={image.images.downsized_large.url} />
+        </div>
         ))}
+
       </div>
     )
   }
