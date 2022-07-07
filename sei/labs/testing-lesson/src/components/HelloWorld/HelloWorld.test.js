9847b15b6e94d1871f78c24e693bddf4af1d8709
Jesse Luna <97872070+jluna101@users.noreply.github.com>
2022-07-06 20:55:38 -0700

    Delete sei directory

--- sei/labs/testing-lesson/src/components/HelloWorld/HelloWorld.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import HelloWorld from './HelloWorld';
-
-test('Should display name of in HelloWorld component', () => {
-    // render the component
-    render(<HelloWorld name={'Jesse'} />);
-    expect(screen.getByText('Jesse')).toBeVisible()
-    expect(screen.getByText('Jesse').tagName).toBe('H1');
-});
